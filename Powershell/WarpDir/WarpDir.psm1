69483e746674d8d9859f44fca9ffbfc26a54b19d
rikardbq <j10rikardb@gmail.com>
2026-04-13 19:45:06 +0200

    update wd, add rename

--- Powershell/WarpDir/WarpDir.psm1
+++ Powershell/WarpDir/WarpDir.psm1
@@ -7,14 +7,16 @@ $ERROR_ALIAS_ALREADY_EXIST = "alias already exist"
 $WD_ROOT = ".wd"
 $WD_DIRS = "dirs"
 $WD_HEADER = "///WD_PWSH_2026"
-$WD_CMDS = @("save", "remove", "list")
+$WD_CMDS = @("save", "rename", "remove", "list")
 
 function wd {
     param (
         [Parameter(Mandatory = $false)]
         $cmd1,
         [Parameter(Mandatory = $false)]
-        $cmd2
+        $cmd2,
+        [Parameter(Mandatory = $false)]
+        $cmd3
     )
     if (-not (Test-Path -Path "$HOME/$WD_ROOT")) {
         New-Item -Path ~/ -Name $WD_ROOT -ItemType "Directory" | Out-Null
@@ -28,7 +30,7 @@ function wd {
         } else {
             $wd_conf = (Get-Content -Path "$HOME/$WD_ROOT/$WD_DIRS").Split("\r\n")
             switch ($cmd1) {
-                $WD_CMDS[0] {
+                $WD_CMDS[0] { #save
                     if (-not $cmd2) {
                         throw $ERROR_ALIAS_NOT_PROVIDED
                     }
@@ -41,7 +43,27 @@ function wd {
                     $global:WD_PREV_PWD[0] = $PWD
                     Write-Output "$cmd2|$PWD" >> "$HOME/$WD_ROOT/$WD_DIRS"
                 }
-                $WD_CMDS[1] {
+                $WD_CMDS[1] { #rename
+                    if (-not $cmd2 -or -not $cmd3) {
+                        throw $ERROR_ALIAS_NOT_PROVIDED
+                    }
+                    $wd_conf_filtered = $wd_conf | Where-Object {
+                        $cmd2 -eq $_.Split("|")[0]
+                    }
+                    if (-join $wd_conf_filtered -eq -join $wd_conf) {
+                        throw $ERROR_ALIAS_NOT_EXIST
+                    }
+                    $wd_conf_mapped = $wd_conf | ForEach-Object {
+                        $wd_alias_split = $_.Split("|")
+                        if ($cmd2 -eq $wd_alias_split[0]) {
+                            "$cmd3|$($wd_alias_split[1])"
+                        } else {
+                            $_
+                        }
+                    }
+                    Write-Output $wd_conf_mapped > "$HOME/$WD_ROOT/$WD_DIRS"
+                }
+                $WD_CMDS[2] { #remove
                     if (-not $cmd2) {
                         throw $ERROR_ALIAS_NOT_PROVIDED
                     }
@@ -53,7 +75,7 @@ function wd {
                     }
                     Write-Output $wd_conf_filtered > "$HOME/$WD_ROOT/$WD_DIRS"
                 }
-                $WD_CMDS[2] {
+                $WD_CMDS[3] { #list
                     (($wd_conf.Length -gt 1) ? $wd_conf[1..($wd_conf.Length - 1)] : ("")) | ForEach-Object {
                         $wd_alias_split = $_.Split("|")
                         [pscustomobject]@{
