b7f16d1b31eb99d3899bf4f00ca78f8272d6cc5a
rikardbq <j10rikardb@gmail.com>
2026-04-13 17:54:06 +0200

    add autocompleter

--- Powershell/WarpDir/WarpDir.psm1
+++ Powershell/WarpDir/WarpDir.psm1
@@ -7,8 +7,9 @@ $ERROR_ALIAS_ALREADY_EXIST = "alias already exist"
 $WD_ROOT = ".wd"
 $WD_DIRS = "dirs"
 $WD_HEADER = "///WD_PWSH_2026"
+$WD_CMDS = @("save", "remove", "list")
 
-function Warp {
+function wd {
     param (
         [Parameter(Mandatory = $false)]
         $cmd1,
@@ -27,7 +28,7 @@ function Warp {
         } else {
             $wd_conf = (Get-Content -Path "$HOME/$WD_ROOT/$WD_DIRS").Split("\r\n")
             switch ($cmd1) {
-                "save" {
+                $WD_CMDS[0] {
                     if (-not $cmd2) {
                         throw $ERROR_ALIAS_NOT_PROVIDED
                     }
@@ -40,7 +41,7 @@ function Warp {
                     $global:WD_PREV_PWD[0] = $PWD
                     Write-Output "$cmd2|$PWD" >> "$HOME/$WD_ROOT/$WD_DIRS"
                 }
-                "remove" {
+                $WD_CMDS[1] {
                     if (-not $cmd2) {
                         throw $ERROR_ALIAS_NOT_PROVIDED
                     }
@@ -52,7 +53,7 @@ function Warp {
                     }
                     Write-Output $wd_conf_filtered > "$HOME/$WD_ROOT/$WD_DIRS"
                 }
-                "list" {
+                $WD_CMDS[2] {
                     (($wd_conf.Length -gt 1) ? $wd_conf[1..($wd_conf.Length - 1)] : ("")) | ForEach-Object {
                         $wd_alias_split = $_.Split("|")
                         [pscustomobject]@{
@@ -78,4 +79,14 @@ function Warp {
         Set-Location $global:WD_PREV_PWD[($PWD.ToString() -eq $global:WD_PREV_PWD[0].ToString()) ? 1 : 0]
     }
 }
-Export-ModuleMember -Function Warp
+Register-ArgumentCompleter -CommandName wd -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+    $wd_conf = (Get-Content -Path "$HOME/$WD_ROOT/$WD_DIRS").Split("\r\n")
+    $completions = $WD_CMDS + (($wd_conf.Length -gt 1) ? $wd_conf[1..($wd_conf.Length - 1)] : ("")) | ForEach-Object {
+        $_.Split("|")[0]
+    }
+    $completions | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+    }
+}
+Export-ModuleMember -Function wd
